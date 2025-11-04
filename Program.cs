4a7f625cf8d2fef92948e48aa3c963f5d4f2e1c0
Elae00 <maissahameza90@gmail.com>
2025-11-04 14:38:19 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int x;
-int y;
-do
-{
-    Console.WriteLine("donner un entier");
-     x = Convert.ToInt32(Console.ReadLine());
 
-} while (x > 0)
+    Console.WriteLine("bonjour");
+
